21f20bcfe6e618e6caa494750a054796327f7142
yingshaoxo <yingshaoxo@gmail.com>
2018-04-16 14:54:31 +0800

    update

--- book/notes/python_script/math/test.py
+++ book/notes/python_script/math/test.py
@@ -2,5 +2,6 @@ from auto_everything import math
 from sympy import *
 
 c = math.Calculator()
-r = c.differential("1/(x + (y ** 2))", "x")
+r = c.differential(, "x")
+r = integrate("1/(x + (y ** 2))")
 print(r)
