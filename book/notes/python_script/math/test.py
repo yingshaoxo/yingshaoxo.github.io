fcfebebce44dd3d01ee44a666aa5f80b93c915a1
yingshaoxo <yingshaoxo@gmail.com>
2018-05-09 19:14:24 +0800

    update

--- book/notes/python_script/math/test.py
+++ book/notes/python_script/math/test.py
@@ -2,5 +2,5 @@ from auto_everything import math
 from sympy import *
 
 c = math.Calculator()
-r = c.differential("-x**2*(tan(x**2/y)**2 + 1)/y**2", "x", use_latex = True)
+r = c.differential("x/(1-x)", "x", use_latex = True)
 print(r)
