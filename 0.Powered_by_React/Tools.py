d8c07d9c252050fc94ef3b0d643c827107bba2b7
yingshaoxo <yingshaoxo@gmail.com>
2018-11-23 18:49:41 +0800

    I'm done today

--- 0.Powered_by_React/Tools.py
+++ 0.Powered_by_React/Tools.py
@@ -10,7 +10,13 @@ class Tools():
     def build(self):
         t.run('yarn build')
         content = io_.read("./build/index.html")
-        content = content.replace("<title>React App</title>", """<title>yingshaoxo | 技术宅</title><meta name="author" content="yingshaoxo" /><meta name="description" content="yingshaoxo, born in 1998, love IT and AI. Want to be a great ML engineer. So I just keep learning and practice everyday." /><meta name="keywords" content="yingshaoxo, YS, 胡英杰, Python, AI, Keras, Tensorflow, React, Javascript, Kotlin, C++" />""")
+        content = content.replace("<title>React App</title>", """
+                <title>yingshaoxo | 技术宅</title>
+                <meta name="author" content="yingshaoxo" />
+                <meta name="description" content="yingshaoxo, born in 1998, love IT and AI. Want to be a great ML engineer. So I just keep learning and practice everyday." />
+                <meta name="keywords" content="yingshaoxo, YS, 胡英杰, Python, AI, Keras, Tensorflow, React, Javascript, Kotlin, C++" />
+                """.replace("\n", "")
+                )
         io_.write("./build/index.html", content)
 
 py.make_it_runnable()
