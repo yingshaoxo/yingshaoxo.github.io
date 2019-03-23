4d4382f51376139e4acea0857035224d17b72969
yingshaoxo <yingshaoxo@gmail.com>
2019-03-23 14:47:53 +0800

    update some info

---
+++ 2.Powered_by_React/build/precache-manifest.60fbcd51f127aa44a8235950fba14f06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2263976792136ee3092d",
+    "url": "./static/js/main.22639767.chunk.js"
+  },
+  {
+    "revision": "4d3c8b998cd04195adc5",
+    "url": "./static/js/1.4d3c8b99.chunk.js"
+  },
+  {
+    "revision": "2263976792136ee3092d",
+    "url": "./static/css/main.c95f1823.chunk.css"
+  },
+  {
+    "revision": "dfb06298f3650ae5afb8df918f62fd65",
+    "url": "./index.html"
+  }
+];
