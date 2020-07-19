207d2f87c7eaa713eb23b83a8573299bfa73ddf3
yingshaoxo <yingshaoxo@gmail.com>
2020-07-20 05:35:56 +0800

    delete a frame

---
+++ 2.Powered_by_React/build/precache-manifest.fcf07ed8ad6bc591b5e6fa63e0d6361a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa909f8b07a575cf379ad90f9016a986",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e376e2cbd2058f90344",
+    "url": "./static/css/main.efe01c60.chunk.css"
+  },
+  {
+    "revision": "b947cd1fe3e39f313f7a",
+    "url": "./static/js/2.7b5f8347.chunk.js"
+  },
+  {
+    "revision": "58f81e817ece7a3b58fac09eaa8504cb",
+    "url": "./static/js/2.7b5f8347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e376e2cbd2058f90344",
+    "url": "./static/js/main.bcac8af8.chunk.js"
+  },
+  {
+    "revision": "fd83ab749d134f836f74",
+    "url": "./static/js/runtime-main.bd9ec8d8.js"
+  }
+]);
