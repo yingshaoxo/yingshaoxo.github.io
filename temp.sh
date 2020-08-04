15f91f53dd92d40fbb61ea6da24e93dc7079b28d
yingshaoxo <yingshaoxo@gmail.com>
2020-08-04 20:28:08 +0800

    it's html repo

--- temp.sh
+++
@@ -1,8 +0,0 @@
-
-        cd 2.Powered_by_React/
-        sudo python3 Tools.py build
-        cd ..
-        sudo git add .
-        sudo git commit -m "add my blog link"
-        sudo git push origin
-        
